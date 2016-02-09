08dd11fc2697aac88eb017c3901c74afee34cd33
Jelte Fennema <github-tech@jeltef.nl>
2016-02-09 18:42:47 +0100

    Initial skeleton python files

---
+++ easyfuse/filesystem.py
@@ -0,0 +1,6 @@
+"""
+This module implements the classes that map to filesystem objects.
+
+..  :copyright: (c) 2016 by Jelte Fennema.
+    :license: MIT, see License for more details.
+"""
---
+++ easyfuse/operations.py
@@ -0,0 +1,6 @@
+"""
+This module implements the class that deals with operations from llfuse.
+
+..  :copyright: (c) 2016 by Jelte Fennema.
+    :license: MIT, see License for more details.
+"""
